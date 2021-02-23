809c86f3a857a69b8ccdc28a7e103d69f365c5f4
Oscar-02 <oscacanas29@gmail.com>
2021-02-22 19:47:35 -0600

    1.0 EJ1 Finished

--- EJ1/EJ1/Program.cs
+++ EJ1/EJ1/Program.cs
@@ -10,6 +10,36 @@ namespace EJ1
     {
         static void Main(string[] args)
         {
+            bool error1 = true;
+            int cant = 0;
+            double precio = 0, total = 0;
+            while (error1 == true)
+            {
+                try
+                {
+                    Console.WriteLine("Por favor ingrese la cantidad de articulos...");
+                    cant = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("Cantidad de Productos Ingresados: " + cant + '\n');
+                    Console.WriteLine("Ingrese el precio unitario de cada producto...");
+                    precio = double.Parse(Console.ReadLine());
+                    Console.Clear();
+                    error1 = false;
+                }
+                catch (Exception error)
+                {
+                    Console.Clear();
+                    Console.WriteLine(error.ToString() + '\n');
+                    error1 = true;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Cantidad de Productos Ingresados: " + cant);
+            Console.WriteLine("Precio Unitario del Producto: $" + precio);
+            total = cant * precio;
+            Console.WriteLine("Total: $" + total + " (Nro. Productos: " + cant + ')');
+            Console.WriteLine("\nPresione cualquier tecla para finalizar...");
+            Console.ReadKey();
         }
     }
 }
